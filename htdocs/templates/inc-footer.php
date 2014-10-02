93b4550c1a805b1ba3689a85a58f7d2abf036035
Michal Hotovec <michal.hotovec@gmail.com>
2014-10-02 14:30:44 +0200

    footer

--- htdocs/templates/inc-footer.php
+++ htdocs/templates/inc-footer.php
@@ -1,8 +1,51 @@
+<footer class="m-page-footer" xmlns="http://www.w3.org/1999/html">
 
-<footer class="m-page-footer">
+    <div class="row mgr-half">
+        <div class="small-6 medium-7 large-4 columns">
+            <h5>OFICIÁLNÍ STRÁNKY VÝSTAVY</h5>
+        </div>
+        <div class="small-6 medium-5 large-3 columns">
+            <h5>OFICIÁLNÍ STRÁNKY VÝSTAVY</h5>
+        </div>
+    </div>
+
     <div class="row">
-        <div class="m-page-footer--content medium-12 columns">
-            <strong>Český pavilon</strong> EXPO2015
+        <div class="small-12 large-4 columns mgr-half">
+            <h5>Pavilon dále podpořili (bronze)</h5>
+        </div>
+    </div>
+
+    <div class="row">
+        <?php for ($i = 1; $i <= 8; $i++) { ?>
+            <article class="small-6 medium-4 large-2 columns m-partner">
+                <header>
+                    <div class="m-item--image"><img src="/assets/ikony/sipka.svg" alt="" /></div>
+                    <h1 class="m-item--hed">sponzor bronze</h1>
+                </header>
+            </article>
+        <?php } ?>
+
+        <!--ending-->
+        <article class="large-2 columns m-partner end">
+            <header>
+                <div class="m-item--image"><img src="/assets/ikony/sipka.svg" alt="" /></div>
+                <h1 class="m-item--hed">sponzor bronze</h1>
+            </header>
+        </article>
+
+    </div>
+
+    <div class="m-page-footer--copyright">
+        <div class="row">
+            <div class="m-page-footer--content small-6 large-4 columns text-left">
+                © 2014 - 17 KOMA Modular
+            </div>
+            <div class="m-page-footer--content small-6 large-4 columns text-center">
+                <strong>Český pavilon</strong> EXPO2015
+            </div>
+            <div class="m-page-footer--content small-12 large-4 columns text-right">
+                Design&&Code OdOka
+            </div>
         </div>
     </div>
 </footer>
