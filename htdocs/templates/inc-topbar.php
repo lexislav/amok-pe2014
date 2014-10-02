f6ca0a4f3357341934770197d8ccbf7f4218f18a
Michal Hotovec <michal.hotovec@gmail.com>
2014-10-02 10:48:33 +0200

    base menu overlay + colors

--- htdocs/templates/inc-topbar.php
+++ htdocs/templates/inc-topbar.php
@@ -2,12 +2,98 @@
     <div class="m-nav--button"></div>
     <div class="m-nav--lang"><i>CZ</i><i>EN</i><i>IT</i></div>
     <div class="m-nav--overlay">
+
         <ul class="m-nav--menu">
-            <li><a href="/index.php"  title="menu1">Home</a></li>
-            <li><a href="/pavilon.php"  title="menu1">Pavilon</a></li>
-            <li><a href="/pavilon.php"  title="menu1">Pavilon</a></li>
-            <li><a href="/pavilon.php"  title="menu1">Pavilon</a></li>
-            <li><a href="/pavilon.php"  title="menu1">Pavilon</a></li>
+
+            <li>
+                <article class="m-item">
+                    <header>
+                        <h1 class="m-item--hed"><a href="/index.php" title="menu1">Home</a></h1>
+                    </header>
+                    <div class="m-item--description">
+                        <p>Adding comment counts to compilation cards (full-width). Tested and merging now, just tagging to let you all know.</p>
+                    </div>
+                    <div class="m-item--meta">
+                        <ul>
+                            <li><a href="/pavilon.php" title="menu1">kategoie</a></li>
+                            <li><a href="/pavilon.php" title="menu1">kategoie</a></li>
+                            <li><a href="/pavilon.php" title="menu1">kategoie</a></li>
+                            <li><a href="/pavilon.php" title="menu1">kategoie</a></li>
+                        </ul>
+                    </div>
+                </article>
+            </li>
+
+            <li>
+                <article class="m-item">
+                    <header>
+                        <h1 class="m-item--hed"><a href="/index.php" title="menu1">Home</a></h1>
+                    </header>
+                    <div class="m-item--description">
+                        <p>Adding comment counts to compilation cards (full-width). Tested and merging now, just tagging to let you all know.</p>
+                    </div>
+
+                </article>
+            </li>
+
+            <li>
+                <article class="m-item">
+                    <header>
+                        <h1 class="m-item--hed"><a href="/index.php" title="menu1">Home</a></h1>
+                    </header>
+                    <div class="m-item--description">
+                        <p>Adding comment counts to compilation cards (full-width). Tested and merging now, just tagging to let you all know.</p>
+                    </div>
+                    <div class="m-item--meta">
+                        <ul>
+                            <li><a href="/pavilon.php" title="menu1">kategoie</a></li>
+                            <li><a href="/pavilon.php" title="menu1">kategoie</a></li>
+                            <li><a href="/pavilon.php" title="menu1">kategoie</a></li>
+                            <li><a href="/pavilon.php" title="menu1">kategoie</a></li>
+                        </ul>
+                    </div>
+                </article>
+            </li>
+
+            <li>
+                <article class="m-item">
+                    <header>
+                        <h1 class="m-item--hed"><a href="/index.php" title="menu1">Home</a></h1>
+                    </header>
+                    <div class="m-item--description">
+                        <p>Adding comment counts to compilation cards (full-width). Tested and merging now, just tagging to let you all know.</p>
+                    </div>
+                    <div class="m-item--meta">
+                        <ul>
+                            <li><a href="/pavilon.php" title="menu1">kategoie</a></li>
+                            <li><a href="/pavilon.php" title="menu1">kategoie</a></li>
+                            <li><a href="/pavilon.php" title="menu1">kategoie</a></li>
+                            <li><a href="/pavilon.php" title="menu1">kategoie</a></li>
+                        </ul>
+                    </div>
+                </article>
+            </li>
+
+            <li>
+                <article class="m-item">
+                    <header>
+                        <h1 class="m-item--hed"><a href="/index.php" title="menu1">Home</a></h1>
+                    </header>
+                    <div class="m-item--description">
+                        <p>Adding comment counts to compilation cards (full-width). Tested and merging now, just tagging to let you all know.</p>
+                    </div>
+                    <div class="m-item--meta">
+                        <ul>
+                            <li><a href="/pavilon.php" title="menu1">kategoie</a></li>
+                            <li><a href="/pavilon.php" title="menu1">kategoie</a></li>
+                            <li><a href="/pavilon.php" title="menu1">kategoie</a></li>
+                            <li><a href="/pavilon.php" title="menu1">kategoie</a></li>
+                        </ul>
+                    </div>
+                </article>
+            </li>
+
         </ul>
+
     </div>
 </nav>
