c4c7b3e8792c031baf07f9d51d3a1ad62413c0df
Michal Hotovec <michal.hotovec@gmail.com>
2014-10-03 15:24:10 +0200

    historie

--- htdocs/historie.php
+++ htdocs/historie.php
@@ -1,100 +1,79 @@
 <?php include "templates/html-header.php"; ?>
-<body class="l-page_pavilon">
+<body class="l-page l-page_history">
 <?php include "templates/inc-topbar.php"; ?>
 
-<div class="tmp-header">
-<div class="row" >
-    <div class="medium-12 columns">
-        slider
-    </div>
-</div>
-</div>
+<div class="m-carousel">
+    <div class="item">
+        <img src="/assets/sky.jpg" alt="slide 1" />
 
+        <div class="orbit-caption">
+            Seznamte se s historií
+            českých pavilonů EXPO
+        </div>
+    </div>
+    <div class="item">
+        <img src="/assets/pavilon-theme.png" alt="slide 1" />
 
+        <div class="orbit-caption">
+            Seznamte se s historií
+            českých pavilonů EXPO
+        </div>
+    </div>
+    <div class="item">
+        <img src="/assets/file-a.jpg" alt="slide 1" />
 
-<div class="row">
-    <div class="small-6 large-3 columns">
-        <img class="th" src="http://foundation.zurb.com/docs/assets/img/examples/satelite.jpg">
-        <h2>1968</h2>
-        <p>Unikátní koncept architektonického řešení umož- ňují principy modulární výstavby. Právě její využití umožní recyklaci pavilonu v budoucnosti.
-        </p>
-    </div>
-    <div class="small-6 large-3 columns">
-        <img class="th" src="http://foundation.zurb.com/docs/assets/img/examples/satelite.jpg">
-        <h2>1968</h2>
-        <p>Lorem ipsum základní výhody modulární výstavby lorem ipsum Hendis alition secus. Ebitem fugitectu- sda dolupta volorei untissi ra sum venda consedi sedist, volorehenes autem ut que volore veles.</p>
-    </div>
-    <div class="small-6 large-3 columns">
-        <img class="th" src="http://foundation.zurb.com/docs/assets/img/examples/satelite.jpg">
-        <h2>1968</h2>
-        <p>Lorem ipsum základní výhody modulární výstavby lorem ipsum Hendis alition secus. Ebitem fugitectu- sda dolupta volorei untissi ra sum venda consedi sedist, volorehenes autem ut que volore veles.</p>
-    </div>
-    <div class="small-6 large-3 columns">
-        <img class="th" src="http://foundation.zurb.com/docs/assets/img/examples/satelite.jpg">
-        <h2>1968</h2>
-        <p>Lorem ipsum základní výhody modulární výstavby lorem ipsum Hendis alition secus. Ebitem fugitectu- sda dolupta volorei untissi ra sum venda consedi sedist, volorehenes autem ut que volore veles.</p>
-    </div>
-</div>
-
-<div class="row">
-    <div class="medium-12 columns tmp-bigimage">
-        image
+        <div class="orbit-caption">
+            Seznamte se s historií
+            českých pavilonů EXPO
+        </div>
     </div>
 </div>
 
 
 <div class="row">
-    <div class="small-6 large-3 columns">
-        <img class="th" src="http://foundation.zurb.com/docs/assets/img/examples/satelite.jpg">
-        <h2>1968</h2>
-        <p>Unikátní koncept architektonického řešení umož- ňují principy modulární výstavby. Právě její využití umožní recyklaci pavilonu v budoucnosti.
-        </p>
-    </div>
-    <div class="small-6 large-3 columns">
-        <img class="th" src="http://foundation.zurb.com/docs/assets/img/examples/satelite.jpg">
-        <h2>1968</h2>
-        <p>Lorem ipsum základní výhody modulární výstavby lorem ipsum Hendis alition secus. Ebitem fugitectu- sda dolupta volorei untissi ra sum venda consedi sedist, volorehenes autem ut que volore veles.</p>
-    </div>
-    <div class="small-6 large-3 columns">
-        <img class="th" src="http://foundation.zurb.com/docs/assets/img/examples/satelite.jpg">
-        <h2>1968</h2>
-        <p>Lorem ipsum základní výhody modulární výstavby lorem ipsum Hendis alition secus. Ebitem fugitectu- sda dolupta volorei untissi ra sum venda consedi sedist, volorehenes autem ut que volore veles.</p>
-    </div>
-    <div class="small-6 large-3 columns">
-        <img class="th" src="http://foundation.zurb.com/docs/assets/img/examples/satelite.jpg">
-        <h2>1968</h2>
-        <p>Lorem ipsum základní výhody modulární výstavby lorem ipsum Hendis alition secus. Ebitem fugitectu- sda dolupta volorei untissi ra sum venda consedi sedist, volorehenes autem ut que volore veles.</p>
-    </div>
+    <?php for ($i = 1; $i <= 20; $i++) { ?>
+        <div class="small-6 large-3 columns">
+            <div class="m-story mm-history">
+                <header>
+                    <div class="m-item--image" style="background-image: url('http://foundation.zurb.com/docs/assets/img/examples/satelite.jpg')">
+                        <a href="single.php"><img src="http://foundation.zurb.com/docs/assets/img/examples/satelite.jpg"></a>
+                    </div>
+
+
+                    <div class="m-item--summary">
+                        <h1 class="m-item--hed">1968</h1>
+                        <div class="m-item--description">Paříž</div>
+                    </div>
+                </header>
+            </div>
+        </div>
+    <?php } ?>
 </div>
 
 
 
-<div class="row">
-    <div class="small-12 medium-12 large-6 columns">
-        <img class="th" src="http://foundation.zurb.com/docs/assets/img/examples/satelite.jpg">
-    </div>
 
-    <div class="small-6 large-3 columns collapse">
-        <img class="th" src="http://foundation.zurb.com/docs/assets/img/examples/satelite.jpg">
-        <h2>1968</h2>
-        <p>Lorem ipsum základní výhody modulární výstavby lorem ipsum Hendis alition secus. Ebitem fugitectu- sda dolupta volorei untissi ra sum venda consedi sedist, volorehenes autem ut que volore veles.</p>
-    </div>
-    <div class="small-6 large-3 columns collapse">
-        <img class="th" src="http://foundation.zurb.com/docs/assets/img/examples/satelite.jpg">
-        <h2>1968</h2>
-        <p>Lorem ipsum základní výhody modulární výstavby lorem ipsum Hendis alition secus. Ebitem fugitectu- sda dolupta volorei untissi ra sum venda consedi sedist, volorehenes autem ut que volore veles.</p>
-    </div>
-</div>
-
-
-<div class="row">
-    <div class="medium-12 columns">
-        <h2>O udržitelné architektuře</h2>
-        <p>Pavilonu stojí přední výrobce modulární výstavby KOMA a jejich dvorní architekti.</p>
-    </div>
-</div>
-
+<?php include "templates/inc-footer-partner.php"; ?>
 <?php include "templates/inc-footer.php"; ?>
+
+<script type="text/javascript" src="components/slick/slick/slick.min.js"></script>
+
+
+<script type="text/javascript">
+
+    $(document).ready(function () {
+        $('.m-carousel').slick({
+            autoplay: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear'
+        });
+
+    });
+
+</script>
+
+<script src="/js/app.js"></script>
 
 </body>
 <?php include "templates/html-footer.php"; ?>
