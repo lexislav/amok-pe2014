ad0a1feae4995976260f72f7eb0c3e051b794829
Michal Hotovec <michal.hotovec@gmail.com>
2014-10-06 00:37:28 +0200

    pavilon schema mobile config

--- htdocs/pavilon.php
+++ htdocs/pavilon.php
@@ -1,23 +1,133 @@
 <?php include "templates/html-header.php"; ?>
 <body class="l-page_pavilon">
-<?php include "templates/inc-topbar.php"; ?>
+<?php // include "templates/inc-topbar.php"; ?>
 
 <div class="row">
     <div class="medium-12 columns">
         <div class="l-schema m-schema">
-            <h1>patra</h1>
+            <div class="m-schema--frame">
+                <div class="m-schema--left">
 
-            <div class="m-schema--slides">
-                <div class="m-schema--image np1"></div>
-                <div class="m-schema--image np2" data-anchor-target="#animAnchor2" data-bottom="opacity:0;" data-center="opacity:1;" data-top="opacity:0;"></div>
-                <div class="m-schema--image np3" data-anchor-target="#animAnchor3" data-bottom="display:none" data-center="opacity:0;display:block" data-top="opacity:1;"></div>
-                <div class="m-schema--image np4" data-anchor-target="#animAnchor3" data-bottom="transform:translate(0px,-600px)" data-center="opacity:1;transform:translate(0px,0px)" data-top="opacity:0;transform:translate(0px,0px)"></div>
-                <div class="m-schema--image np6" data-anchor-target="#animAnchor4" data-bottom="transform:translate(0px,-600px)" data-center="opacity:1;transform:translate(0px,0px)"></div>
-                <div class="m-schema--image np7" data-anchor-target="#animAnchor5" data-bottom="transform:translate(0px,-600px)" data-center="transform:translate(0px,0px)"></div>
-                <div class="m-schema--overlay"></div>
+
+                    <div class="m-schema--label label1" data-anchor-target="#animAnchor2" data-bottom="opacity:0;" data-center="opacity:1;" data-top="opacity:0;">
+                        <h2 class="m-item--hed"><strong>Přízemí:</strong> Země – Voda</h2>
+                        <div class="m-item--description">
+                            Relaxační vodní plocha, pohodlná lehátka i výtečná restaurace připomenou návštěvníkům českou pohostinnost hned po vstupu do areálu.
+                        </div>
+                        <ul class="mm-red">
+                            <li>
+                                <h3><span>1.</span>URBÁNNÍ BAZÉN</h3>
+
+                                <p>Český sen o moři symbolizuje urbánní bazén pavilonu. Mělký bazén zároveň splní úlohu relaxační plochy.</p>
+                            </li>
+                            <li>
+                                <h3><span>2.</span>BUFET</h3>
+
+                                <p>Občerstvení pro všechny kteří chtějí skočit do vody i všem ostatním, kteří relaxují u bazénu.</p>
+                            </li>
+                            <li>
+                                <h3><span>3.</span>RESTAURACE</h3>
+
+                                <p>O pohodlí a knedlíky nebude v pavilonu nouze. Zázemí restaurace vyhovuje nejpřísnějším hygienickým předpisům i dostatečným pohodlím.</p>
+                            </li>
+                            <li>
+                                <h3><span>4.</span>AMFITEÁTR</h3>
+
+                                <p>Schody vedoucí do prvního patra nejsou obyčejné schody. Slouží také k odpočinku ve stínku i jako přednáškový sál!</p>
+                            </li>
+                        </ul>
+                    </div>
+
+
+                    <div class="m-schema--label label2" data-anchor-target="#animAnchor3" data-bottom="opacity:0;" data-center="opacity:1;" data-top="opacity:0;">
+                        <h2 class="m-item--hed">1. patro: Země příběhů a fantazie</h2>
+                        <div class="m-item--description">
+                            V první části expozice se každý měsíc představí jeden kraj České republiky vlastní výstavou. Dominantu prostoru tvoří Český les.
+                        </div>
+                        <ul class="mm-green">
+                            <li>
+                                <h3><span>1.</span>NÁRODNÍ EXPOZICE</h3>
+
+                                <p>V národní expozici s názvem “Země příběhů a fantazie” se v průběhu konání výstavy vystřídají expozice z 8 vybraných krajů České republiky</p>
+                            </li>
+                            <li>
+                                <h3><span>2.</span>LABORATŘ TICHA / ČESKÝ LES</h3>
+
+                                <p>Speciální umělecký objekt reprezentující české umění</p>
+                            </li>
+                            <li>
+                                <h3><span>3.</span>ZÁZEMÍ</h3>
+
+                                <p>První patro ukrývá zázemí pro personál i návštěvy.</p>
+                            </li>
+
+                        </ul>
+                    </div>
+
+
+
+                    <div class="m-schema--label label3" data-anchor-target="#animAnchor4" data-bottom="opacity:0;" data-center="opacity:1;" data-top="opacity:0;">
+                        <h2 class="m-item--hed">2. patro: Laboratoř života</h2>
+                        <div class="m-item--description">
+                            Patru dominuje originální stálá expozice z dílen studentů ČVUT, umělců a tak dále, a tak dále.
+                        </div>
+                        <ul class="mm-blue">
+                            <li>
+                                <h3><span>1.</span>TRVALÁ EXPOZICE</h3>
+
+                                <p>Stálá expozice bude prezentovat výsledky vědy doplněné uměleckými předměty předních českých umělců.</p>
+                            </li>
+                            <li>
+                                <h3><span>2.</span>UMĚLECKÝ OBJEKT</h3>
+
+                                <p>Kinetická plastika společnosti Lasvit, která byla premiérově představena na výstavě v Miláně 2014 Salone Internazionale del Mobile.</p>
+                            </li>
+                            <li>
+                                <h3><span>3.</span>V.I.P. SALONEK</h3>
+
+                                <p>VIP salonek, který bude sloužit partnerům k prezentacím, konferencím a schůzkám.</p>
+                            </li>
+
+                        </ul>
+                    </div>
+
+
+
+                    <div class="m-schema--label label4" data-anchor-target="#animAnchor5" data-bottom="opacity:0;" data-center="opacity:1;" data-top="opacity:0;">
+                        <h2 class="m-item--hed">Střecha: Zahrada & relax</h2>
+                        <div class="m-item--description">
+                            Střechu pavilonu pokrývá zeleň. Poskytuje výhled do areálu, místo k nadechnutí, zklidnění i relaxaci. Poskytuje prostor pro společenské akce partnerů.
+                        </div>
+                        <ul class="mm-yellow">
+                            <li>
+                                <h3><span>1.</span>STŘEŠNÍ ZAHRADA</h3>
+
+                                <p>Relaxační plocha plná zeleně poskytne návštěvníkům přehled o celém areálu EXPO2015 v Miláně i příjemný posez a tak dále.</p>
+                            </li>
+                            <li>
+                                <h3><span>2.</span>UMĚLECKÝ OBJEKT</h3>
+
+                                <p>Zatím tajemstvím opředená expozice (Diaz) reprezentující stav umění v Česku.</p>
+                            </li>
+
+
+                        </ul>
+                    </div>
+
+
+                </div>
+                <div class="m-schema--right">
+                    <div class="m-schema--slides">
+                        <div class="m-schema--image np1"></div>
+                        <div class="m-schema--image np2" data-anchor-target="#animAnchor2" data-bottom="opacity:0;" data-center="opacity:1;" data-top="opacity:0;"></div>
+                        <div class="m-schema--image np3" data-anchor-target="#animAnchor3" data-bottom="display:none" data-center="opacity:0;display:block" data-top="opacity:1;"></div>
+                        <div class="m-schema--image np4" data-anchor-target="#animAnchor3" data-bottom="transform:translate(0px,-600px)" data-center="opacity:1;transform:translate(0px,0px)" data-top="opacity:0;transform:translate(0px,0px)"></div>
+                        <div class="m-schema--image np6" data-anchor-target="#animAnchor4" data-bottom="transform:translate(0px,-600px)" data-center="opacity:1;transform:translate(0px,0px)"></div>
+                        <div class="m-schema--image np7" data-anchor-target="#animAnchor5" data-bottom="transform:translate(0px,-600px)" data-center="transform:translate(0px,0px)"></div>
+                        <div class="m-schema--overlay"></div>
+                    </div>
+                </div>
             </div>
-
-
         </div>
     </div>
 </div>
@@ -125,12 +235,11 @@
         </div>
 
 
-    <?php include "templates/inc-footer-partner.php"; ?>
-    <?php include "templates/inc-footer.php"; ?>
+        <?php include "templates/inc-footer-partner.php"; ?>
+        <?php include "templates/inc-footer.php"; ?>
 
     </div>
 </div>
-
 
 
 <script type="text/javascript" src="components/slick/slick/slick.min.js"></script>
