3be872db3654dd1ab19d88ef0ea7294bb0c36ab6
Michal Hotovec <michal.hotovec@gmail.com>
2014-10-06 13:00:11 +0200

    fix carousel

--- htdocs/pavilon.php
+++ htdocs/pavilon.php
@@ -1,147 +1,146 @@
 <?php include "templates/html-header.php"; ?>
 <body class="l-page_pavilon">
-<?php // include "templates/inc-topbar.php"; ?>
+<?php include "templates/inc-topbar.php"; ?>
 
-<div class="row">
-    <div class="medium-12 columns">
-        <div class="l-schema m-schema">
-            <div class="m-schema--frame">
-                <div class="m-schema--left">
-
-
-                    <div class="m-schema--label label1" data-anchor-target="#animAnchor2" data-bottom="opacity:0;" data-center="opacity:1;" data-top="opacity:0;">
-                        <h2 class="m-item--hed"><strong>Přízemí:</strong> Země – Voda</h2>
-
-                        <div class="m-item--description">
-                            Relaxační vodní plocha, pohodlná lehátka i výtečná restaurace připomenou návštěvníkům českou pohostinnost hned po vstupu do areálu.
-                        </div>
-                        <ul class="mm-red">
-                            <li>
-                                <h3><span>1.</span>URBÁNNÍ BAZÉN</h3>
-
-                                <p>Český sen o moři symbolizuje urbánní bazén pavilonu. Mělký bazén zároveň splní úlohu relaxační plochy.</p>
-                            </li>
-                            <li>
-                                <h3><span>2.</span>BUFET</h3>
-
-                                <p>Občerstvení pro všechny kteří chtějí skočit do vody i všem ostatním, kteří relaxují u bazénu.</p>
-                            </li>
-                            <li>
-                                <h3><span>3.</span>RESTAURACE</h3>
-
-                                <p>O pohodlí a knedlíky nebude v pavilonu nouze. Zázemí restaurace vyhovuje nejpřísnějším hygienickým předpisům i dostatečným pohodlím.</p>
-                            </li>
-                            <li>
-                                <h3><span>4.</span>AMFITEÁTR</h3>
-
-                                <p>Schody vedoucí do prvního patra nejsou obyčejné schody. Slouží také k odpočinku ve stínku i jako přednáškový sál!</p>
-                            </li>
-                        </ul>
-                    </div>
+<div class="l-schema m-schema">
+    <div class="m-schema--decoration mm-top"></div>
+    <div class="m-schema--decoration mm-bottom"></div>
+    <div class="m-schema--frame">
+        <div class="m-schema--left">
 
 
-                    <div class="m-schema--label label2" data-anchor-target="#animAnchor3" data-bottom="opacity:0;" data-center="opacity:1;" data-top="opacity:0;">
-                        <h2 class="m-item--hed">1. patro: Země příběhů a fantazie</h2>
+            <div class="m-schema--label label1" data-anchor-target="#animAnchor2" data-bottom="opacity:0;" data-center="opacity:1;" data-top="opacity:0;">
+                <h2 class="m-item--hed"><strong>Přízemí:</strong> Země – Voda</h2>
 
-                        <div class="m-item--description">
-                            V první části expozice se každý měsíc představí jeden kraj České republiky vlastní výstavou. Dominantu prostoru tvoří Český les.
-                        </div>
-                        <ul class="mm-green">
-                            <li>
-                                <h3><span>1.</span>NÁRODNÍ EXPOZICE</h3>
-
-                                <p>V národní expozici s názvem “Země příběhů a fantazie” se v průběhu konání výstavy vystřídají expozice z 8 vybraných krajů České republiky</p>
-                            </li>
-                            <li>
-                                <h3><span>2.</span>LABORATŘ TICHA / ČESKÝ LES</h3>
-
-                                <p>Speciální umělecký objekt reprezentující české umění</p>
-                            </li>
-                            <li>
-                                <h3><span>3.</span>ZÁZEMÍ</h3>
-
-                                <p>První patro ukrývá zázemí pro personál i návštěvy.</p>
-                            </li>
-
-                        </ul>
-                    </div>
-
-
-                    <div class="m-schema--label label3" data-anchor-target="#animAnchor4" data-bottom="opacity:0;" data-center="opacity:1;" data-top="opacity:0;">
-                        <h2 class="m-item--hed">2. patro: Laboratoř života</h2>
-
-                        <div class="m-item--description">
-                            Patru dominuje originální stálá expozice z dílen studentů ČVUT, umělců a tak dále, a tak dále.
-                        </div>
-                        <ul class="mm-blue">
-                            <li>
-                                <h3><span>1.</span>TRVALÁ EXPOZICE</h3>
-
-                                <p>Stálá expozice bude prezentovat výsledky vědy doplněné uměleckými předměty předních českých umělců.</p>
-                            </li>
-                            <li>
-                                <h3><span>2.</span>UMĚLECKÝ OBJEKT</h3>
-
-                                <p>Kinetická plastika společnosti Lasvit, která byla premiérově představena na výstavě v Miláně 2014 Salone Internazionale del Mobile.</p>
-                            </li>
-                            <li>
-                                <h3><span>3.</span>V.I.P. SALONEK</h3>
-
-                                <p>VIP salonek, který bude sloužit partnerům k prezentacím, konferencím a schůzkám.</p>
-                            </li>
-
-                        </ul>
-                    </div>
-
-
-                    <div class="m-schema--label label4" data-anchor-target="#animAnchor5" data-bottom="opacity:0;" data-center="opacity:1;">
-                        <h2 class="m-item--hed">Střecha: Zahrada & relax</h2>
-
-                        <div class="m-item--description">
-                            Střechu pavilonu pokrývá zeleň. Poskytuje výhled do areálu, místo k nadechnutí, zklidnění i relaxaci. Poskytuje prostor pro společenské akce partnerů.
-                        </div>
-                        <ul class="mm-yellow">
-                            <li>
-                                <h3><span>1.</span>STŘEŠNÍ ZAHRADA</h3>
-
-                                <p>Relaxační plocha plná zeleně poskytne návštěvníkům přehled o celém areálu EXPO2015 v Miláně i příjemný posez a tak dále.</p>
-                            </li>
-                            <li>
-                                <h3><span>2.</span>UMĚLECKÝ OBJEKT</h3>
-
-                                <p>Zatím tajemstvím opředená expozice (Diaz) reprezentující stav umění v Česku.</p>
-                            </li>
-
-
-                        </ul>
-                    </div>
-
-
+                <div class="m-item--description">
+                    Relaxační vodní plocha, pohodlná lehátka i výtečná restaurace připomenou návštěvníkům českou pohostinnost hned po vstupu do areálu.
                 </div>
-                <div class="m-schema--right">
-                    <div class="m-schema--slides">
-                        <div class="m-schema--image np1"></div>
-                        <div class="m-schema--image np2" data-anchor-target="#animAnchor2" data-bottom="opacity:0;" data-center="opacity:1;" data-top="opacity:0;"></div>
-                        <div class="m-schema--image np3" data-anchor-target="#animAnchor3" data-bottom="display:none" data-center="opacity:0;display:block" data-top="opacity:1;"></div>
-                        <div class="m-schema--image np4" data-anchor-target="#animAnchor3" data-bottom="transform:translate(0px,-600px)" data-center="opacity:1;transform:translate(0px,0px)" data-top="opacity:0;transform:translate(0px,0px)"></div>
-                        <div class="m-schema--image np6" data-anchor-target="#animAnchor4" data-bottom="transform:translate(0px,-600px)" data-center="opacity:1;transform:translate(0px,0px)"></div>
-                        <div class="m-schema--image np7" data-anchor-target="#animAnchor5" data-bottom="transform:translate(0px,-600px)" data-center="transform:translate(0px,0px)"></div>
-                        <div class="m-schema--overlay"></div>
-                    </div>
+                <ul class="mm-red">
+                    <li>
+                        <h3><span>1.</span>URBÁNNÍ BAZÉN</h3>
+
+                        <p>Český sen o moři symbolizuje urbánní bazén pavilonu. Mělký bazén zároveň splní úlohu relaxační plochy.</p>
+                    </li>
+                    <li>
+                        <h3><span>2.</span>BUFET</h3>
+
+                        <p>Občerstvení pro všechny kteří chtějí skočit do vody i všem ostatním, kteří relaxují u bazénu.</p>
+                    </li>
+                    <li>
+                        <h3><span>3.</span>RESTAURACE</h3>
+
+                        <p>O pohodlí a knedlíky nebude v pavilonu nouze. Zázemí restaurace vyhovuje nejpřísnějším hygienickým předpisům i dostatečným pohodlím.</p>
+                    </li>
+                    <li>
+                        <h3><span>4.</span>AMFITEÁTR</h3>
+
+                        <p>Schody vedoucí do prvního patra nejsou obyčejné schody. Slouží také k odpočinku ve stínku i jako přednáškový sál!</p>
+                    </li>
+                </ul>
+            </div>
+
+
+            <div class="m-schema--label label2" data-anchor-target="#animAnchor3" data-bottom="opacity:0;" data-center="opacity:1;" data-top="opacity:0;">
+                <h2 class="m-item--hed">1. patro: Země příběhů a fantazie</h2>
+
+                <div class="m-item--description">
+                    V první části expozice se každý měsíc představí jeden kraj České republiky vlastní výstavou. Dominantu prostoru tvoří Český les.
                 </div>
+                <ul class="mm-green">
+                    <li>
+                        <h3><span>1.</span>NÁRODNÍ EXPOZICE</h3>
+
+                        <p>V národní expozici s názvem “Země příběhů a fantazie” se v průběhu konání výstavy vystřídají expozice z 8 vybraných krajů České republiky</p>
+                    </li>
+                    <li>
+                        <h3><span>2.</span>LABORATŘ TICHA / ČESKÝ LES</h3>
+
+                        <p>Speciální umělecký objekt reprezentující české umění</p>
+                    </li>
+                    <li>
+                        <h3><span>3.</span>ZÁZEMÍ</h3>
+
+                        <p>První patro ukrývá zázemí pro personál i návštěvy.</p>
+                    </li>
+
+                </ul>
+            </div>
+
+
+            <div class="m-schema--label label3" data-anchor-target="#animAnchor4" data-bottom="opacity:0;" data-center="opacity:1;" data-top="opacity:0;">
+                <h2 class="m-item--hed">2. patro: Laboratoř života</h2>
+
+                <div class="m-item--description">
+                    Patru dominuje originální stálá expozice z dílen studentů ČVUT, umělců a tak dále, a tak dále.
+                </div>
+                <ul class="mm-blue">
+                    <li>
+                        <h3><span>1.</span>TRVALÁ EXPOZICE</h3>
+
+                        <p>Stálá expozice bude prezentovat výsledky vědy doplněné uměleckými předměty předních českých umělců.</p>
+                    </li>
+                    <li>
+                        <h3><span>2.</span>UMĚLECKÝ OBJEKT</h3>
+
+                        <p>Kinetická plastika společnosti Lasvit, která byla premiérově představena na výstavě v Miláně 2014 Salone Internazionale del Mobile.</p>
+                    </li>
+                    <li>
+                        <h3><span>3.</span>V.I.P. SALONEK</h3>
+
+                        <p>VIP salonek, který bude sloužit partnerům k prezentacím, konferencím a schůzkám.</p>
+                    </li>
+
+                </ul>
+            </div>
+
+
+            <div class="m-schema--label label4" data-anchor-target="#animAnchor5" data-bottom="opacity:0;" data-center="opacity:1;">
+                <h2 class="m-item--hed">Střecha: Zahrada & relax</h2>
+
+                <div class="m-item--description">
+                    Střechu pavilonu pokrývá zeleň. Poskytuje výhled do areálu, místo k nadechnutí, zklidnění i relaxaci. Poskytuje prostor pro společenské akce partnerů.
+                </div>
+                <ul class="mm-yellow">
+                    <li>
+                        <h3><span>1.</span>STŘEŠNÍ ZAHRADA</h3>
+
+                        <p>Relaxační plocha plná zeleně poskytne návštěvníkům přehled o celém areálu EXPO2015 v Miláně i příjemný posez a tak dále.</p>
+                    </li>
+                    <li>
+                        <h3><span>2.</span>UMĚLECKÝ OBJEKT</h3>
+
+                        <p>Zatím tajemstvím opředená expozice (Diaz) reprezentující stav umění v Česku.</p>
+                    </li>
+
+
+                </ul>
+            </div>
+
+
+        </div>
+        <div class="m-schema--right">
+            <div class="m-schema--slides">
+                <div class="m-schema--image np1"></div>
+                <div class="m-schema--image np2" data-anchor-target="#animAnchor2" data-bottom="opacity:0;" data-center="opacity:1;" data-top="opacity:0;"></div>
+                <div class="m-schema--image np3" data-anchor-target="#animAnchor3" data-bottom="display:none" data-center="opacity:0;display:block" data-top="opacity:1;"></div>
+                <div class="m-schema--image np4" data-anchor-target="#animAnchor3" data-bottom="transform:translate(0px,-600px)" data-center="opacity:1;transform:translate(0px,0px)" data-top="opacity:0;transform:translate(0px,0px)"></div>
+                <div class="m-schema--image np6" data-anchor-target="#animAnchor4" data-bottom="transform:translate(0px,-600px)" data-center="opacity:1;transform:translate(0px,0px)"></div>
+                <div class="m-schema--image np7" data-anchor-target="#animAnchor5" data-bottom="transform:translate(0px,-600px)" data-center="transform:translate(0px,0px)"></div>
+                <div class="m-schema--overlay"></div>
             </div>
         </div>
     </div>
 </div>
 
+
 <div id="skrollr-body">
     <div class="m-carousel">
-        <div class="item">
+        <div class="m-carousel--item">
             <img src="/assets/photos/pavilon/s32.jpg" alt="slide 1" />
 
             <div class="orbit-caption">
-                <div>Prohlédněte si český pavilon</div>
-                <div class="fnt-primary-body-m" style="margin-top: 200px;">
+                <div class="m-item--hed">Prohlédněte si český pavilon</div>
+                <div class="m-item--description fnt-primary-body-m" style="margin-top: 200px;">
                     Český pavilon reprezentuje český sen o moři,
                     česko jako střechu Evropy.
                     Umístěn je hned u hlavního vchodu
@@ -153,12 +152,12 @@
                 <img src="/assets/ikony/arrow-down.png" alt="" />
             </div>
         </div>
-        <div class="item">
+        <div class="m-carousel--item">
             <img src="/assets/photos/pavilon/s33.jpg" alt="slide 1" />
 
             <div class="orbit-caption">
-                <div>Prohlédněte si český pavilon</div>
-                <div class="fnt-primary-body-m" style="margin-top: 200px;">
+                <div class="m-item--hed">Prohlédněte si český pavilon</div>
+                <div class="m-item--description fnt-primary-body-m" style="margin-top: 200px;">
                     Český pavilon reprezentuje český sen o moři,
                     česko jako střechu Evropy.
                     Umístěn je hned u hlavního vchodu
@@ -170,12 +169,12 @@
                 <img src="/assets/ikony/arrow-down.png" alt="" />
             </div>
         </div>
-        <div class="item">
+        <div class="m-carousel--item">
             <img src="/assets/photos/pavilon/s34.jpg" alt="slide 1" />
 
             <div class="orbit-caption">
-                <div>Prohlédněte si český pavilon</div>
-                <div class="fnt-primary-body-m" style="margin-top: 200px;">
+                <div class="m-item--hed">Prohlédněte si český pavilon</div>
+                <div class="m-item--description fnt-primary-body-m" style="margin-top: 200px;">
                     Český pavilon reprezentuje český sen o moři,
                     česko jako střechu Evropy.
                     Umístěn je hned u hlavního vchodu
@@ -190,18 +189,13 @@
     </div>
 
     <div class="m-schema-spacer"></div>
-    <div class="m-schema-control" id="animAnchor2">a2</div>
+    <div class="m-schema-control" id="animAnchor2"><span>a2</span></div>
     <div class="m-schema-spacer"></div>
-    <div class="m-schema-control" id="animAnchor3">a3</div>
+    <div class="m-schema-control" id="animAnchor3"><span>a3</span></div>
     <div class="m-schema-spacer"></div>
-    <div class="m-schema-control" id="animAnchor4">a4</div>
+    <div class="m-schema-control" id="animAnchor4"><span>a4</span></div>
     <div class="m-schema-spacer"></div>
-    <div class="m-schema-control" id="animAnchor5">a5</div>
-
-
-    <div>
-        only xsmall
-    </div>
+    <div class="m-schema-control" id="animAnchor5"><span>a5</span></div>
 
 
     <div class="skrollr-wrap">
@@ -231,7 +225,7 @@
                                 </svg>
                             </div>
                         </a>
-                    </footer >
+                    </footer>
 
 
                 </div>
@@ -262,7 +256,7 @@
 
             <div class="medium-6 columns">
 
-                <article class="m-story" >
+                <article class="m-story">
                     <header>
                         <div class="small-12 columns">
                             <h1 class="m-item--hed">Budoucnost pavilonu</h1>
@@ -313,9 +307,9 @@
 
 
     $(document).ready(function () {
-        $(".m-carousel > .item").css('max-height',$(window).height());
-        $(".m-carousel").css('max-height',$(window).height());
-        $(".m-carousel").css('height',$(window).height());
+        $(".m-carousel > .m-carousel--item").css('max-height', $(window).height());
+        $(".m-carousel").css('max-height', $(window).height());
+        $(".m-carousel").css('height', $(window).height());
 
         $('.m-carousel').slick({
             autoplay: true,
